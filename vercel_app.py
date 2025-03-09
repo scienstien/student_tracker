c135d1ca96ff14bc4361486c5e5b9ff1aae88932
chayan <chayan23rd@gmail.com>
2025-03-10 00:30:51 +0530

    Fix: Update build script to use python -m pip

--- vercel_app.py
+++ vercel_app.py
@@ -1,4 +1,11 @@
-from student_tracker.wsgi import application
+import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-# Init app
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_tracker.settings')
+
+application = get_wsgi_application()
+application = WhiteNoise(application)
+application.add_files('staticfiles', prefix='static/')
+
 app = application 
